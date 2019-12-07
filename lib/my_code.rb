49a53adfa3c1a95ec00655f932094d24a29702c8
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-12-07 20:40:17 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,25 @@
-# Your Code Here
+def map(source)
+  new = []
+  i = 0 
+  while i < source.length do 
+    new << yield(source[i])
+  i += 1
+  end 
+  return new
+end
+
+
+def reduce(source, sv=nil)
+  if sv
+    sum = sv
+    i = 0 
+  else
+    sum = source[0]
+    i = 1
+  end
+  while i < source.length do 
+   sum = yield(sum, source[i])
+    i += 1 
+  end
+  return sum
+end
